523c5af4b18ede8c30186b6a283ce416d382e058
stromka <kstromhaug@gmail.com>
2025-12-22 13:25:10 -0700

    finished with part 1

--- day-02/src/part1.rs
+++ day-02/src/part1.rs
@@ -21,8 +21,22 @@ pub fn process_input(data: String) -> Vec<(usize, usize)> {
     return pairs
 }
 
+pub fn is_valid_id(id: usize) -> bool {
+    let id_str = id.to_string();
+
+    // an odd length is automatically valid
+    if id_str.len() % 2 != 0 { return true }
+
+    let (half_1, half_2) = id_str.split_at(id_str.len() / 2);
+
+    if half_1 == half_2 {
+        return false
+    }
+    true
+}
+
 #[tracing::instrument]
-pub fn process(path: &Path) -> anyhow::Result<String> {
+pub fn process(path: &Path) -> anyhow::Result<usize> {
     let text = read_txt(&path);
     let row = text.iter().next().unwrap().clone();
     let input = process_input(row);
@@ -30,8 +44,17 @@ pub fn process(path: &Path) -> anyhow::Result<String> {
     // iterate through numbers in each range. 
     // convert to string and split in half
     // see if the halves are the same
+    let mut sum = 0;
 
-    Ok("sup".to_string())
+    input.iter().for_each(|(min, max)| {
+        for id in *min..=*max {
+            if !is_valid_id(id) {
+                sum += id
+            }
+        }
+    });
+
+    Ok(sum)
 }
 
 #[cfg(test)]
@@ -42,7 +65,7 @@ mod tests {
     fn test_process() -> anyhow::Result<()> {
         let input = Path::new("./../inputs/day2_part1_sample.csv");
 
-        assert_eq!("sup", process(input)?);
+        assert_eq!(1227775554, process(input)?);
         Ok(())
     }
 
@@ -51,6 +74,28 @@ mod tests {
         let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124".to_string();
 
         assert_eq!(11, process_input(input).len());
+        Ok(())
+    }
+
+    #[test]
+    fn test_process_full() -> anyhow::Result<()> {
+        let input = Path::new("./../inputs/day2_part1.csv");
+
+        assert_eq!(1227775554, process(input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_is_valid_id_1() -> anyhow::Result<()> {
+        assert!(!is_valid_id(11));
+        assert!(is_valid_id(12));
+
+        assert!(is_valid_id(101));
+        assert!(is_valid_id(1122));
+        assert!(!is_valid_id(1212));
+
+        assert!(!is_valid_id(1188511885));
+
         Ok(())
     }
 }
