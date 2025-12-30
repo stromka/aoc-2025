7d257ec723bae9a19c00f640f53771a62dc09687
stromka <kstromhaug@gmail.com>
2025-12-30 14:48:22 -0700

    add main file and benchmarks

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # aoc-2025
 
+## Day 3
+
+### Part 1
+
+```
+hyperfine --warmup 3 './target/release/day-03 --part 1'
+Benchmark 1: ./target/release/day-03 --part 1
+  Time (mean ± σ):     809.0 µs ± 1588.3 µs    [User: 594.7 µs, System: 327.1 µs]
+  Range (min … max):     0.0 µs … 16233.8 µs    260 runs
+```
+
+### Part 2
+
+```
+hyperfine --warmup 3 './target/release/day-03 --part 2'
+Benchmark 1: ./target/release/day-03 --part 2
+  Time (mean ± σ):     970.7 µs ± 1499.9 µs    [User: 689.1 µs, System: 423.3 µs]
+  Range (min … max):     0.0 µs … 18546.2 µs    545 runs
+```
+
 ## Day 6
 
 Nothing pretty here
---
+++ day-03/src/main.rs
@@ -0,0 +1,25 @@
+use day_03::part1::process as process_part1;
+use day_03::part2::process as process_part2;
+use std::path::Path;
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    part: usize,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if args.part == 1 {
+        let res = process_part1(&Path::new("../inputs/day3.csv"));
+        assert_eq!(res.unwrap(), 17031);
+    } else if args.part == 2 {
+        let res = process_part2(&Path::new("../inputs/day3.csv"));
+        assert_eq!(res.unwrap(), 168575096286051);
+    }
+}
