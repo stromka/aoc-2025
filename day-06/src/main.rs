1a9db0b6a4302514d663f2f6e40fdbd5c564ad8f
stromka <kstromhaug@gmail.com>
2025-12-30 11:25:51 -0700

    fix the main script

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,5 +1,5 @@
-use daily_template::part1::process as process_part1;
-use daily_template::part2::process as process_part2;
+use day_06::part1::process as process_part1;
+use day_06::part2::process as process_part2;
 use std::path::Path;
 use clap::Parser;
 
@@ -16,8 +16,8 @@ fn main() {
     let args = Args::parse();
 
     if args.part == 1 {
-        let res = process_part1(&Path::new("../inputs/day5.txt"));
+        let res = process_part1(&Path::new("../inputs/day6.txt"));
     } else if args.part == 2 {
-        let res = process_part2(&Path::new("../inputs/day5.txt"));
+        let res = process_part2(&Path::new("../inputs/day6.txt"));
     }
 }
